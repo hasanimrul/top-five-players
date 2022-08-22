3f3552a4f87b8a5bdecff2e9f15e5ba09853c9c1
hasanimrul <mohammadhasan.imrul@gmail.com>
2022-08-22 21:28:39 +0600

    per player expences calculation completed

--- js/players.js
+++ js/players.js
@@ -1,26 +1,47 @@
 const players = [];
+let numberOfPlayer = 0;
 
-function displayList(){
-    const displayPlayers = document.getElementById('display-players');
-    displayPlayers.textContent = '';
-    for(let i = 0; i < players.length; i++){
-        
-        if(i > 4){
-            alert('Maximum added exceeded')
-            return;
-        }
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-        <th>${i+1}.</th>
+function displayList() {
+  const displayPlayers = document.getElementById("display-players");
+  displayPlayers.textContent = "";
+  
+  for (let i = 0; i < players.length; i++) {
+    if (i > 4) {
+      alert("Maximum added exceeded");
+      return;
+    }
+    numberOfPlayer = i + 1;
+
+    const selectedPlayers = document.getElementById("selected-players");
+    selectedPlayers.innerText = numberOfPlayer + 1;
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+        <th>${i + 1}.</th>
         <td>${players[i]}</td>
         `;
-        
-        displayPlayers.appendChild(tr);
-    }
+
+    displayPlayers.appendChild(tr);
+  }
 }
-function addToList(element){
-    const playerName = element.parentNode.parentNode.children[0].innerText;
-    players.push(playerName);
-    displayList();
+function addToList(element) {
+  const playerName = element.parentNode.parentNode.children[0].innerText;
+  players.push(playerName);
+  displayList();
 }
 
+function getInputFieldValueById(elementId) {
+  const inputFieldElement = document.getElementById(elementId);
+  const inputFieldValueStr = inputFieldElement.value;
+  const inputFieldValue = parseInt(inputFieldValueStr);
+  return inputFieldValue;
+}
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const perPlayerExpence = getInputFieldValueById("per-player-expence");
+
+  const expencesAsPlayerNum = perPlayerExpence * numberOfPlayer;
+
+  const playerExpences = document.getElementById("player-expences");
+  playerExpences.innerText = expencesAsPlayerNum;
+});
