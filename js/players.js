68cd165b3fde56ee34b90203660319b528bc1ed2
hasanimrul <mohammadhasan.imrul@gmail.com>
2022-08-22 23:53:57 +0600

    manager couch abd total calculation is completed

--- js/players.js
+++ js/players.js
@@ -4,13 +4,13 @@ let numberOfPlayer = 0;
 function displayList() {
   const displayPlayers = document.getElementById("display-players");
   displayPlayers.textContent = "";
-  
+
   for (let i = 0; i < players.length; i++) {
     if (i > 4) {
       alert("Maximum added exceeded");
       return;
     }
-    numberOfPlayer = i + 1;
+    numberOfPlayer = i;
 
     const selectedPlayers = document.getElementById("selected-players");
     selectedPlayers.innerText = numberOfPlayer + 1;
@@ -24,6 +24,7 @@ function displayList() {
     displayPlayers.appendChild(tr);
   }
 }
+
 function addToList(element) {
   const playerName = element.parentNode.parentNode.children[0].innerText;
   players.push(playerName);
@@ -34,14 +35,39 @@ function getInputFieldValueById(elementId) {
   const inputFieldElement = document.getElementById(elementId);
   const inputFieldValueStr = inputFieldElement.value;
   const inputFieldValue = parseInt(inputFieldValueStr);
+  
+  if(isNaN(inputFieldValue)){
+    alert('Please enter number')
+    return;
+  }
   return inputFieldValue;
 }
 
+function getTextValueById(elementId){
+    const textElement = document.getElementById(elementId);
+    const textElementStr = textElement.innerText;
+    const text = parseInt(textElementStr);
+    return text;
+}
+
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const perPlayerExpence = getInputFieldValueById("per-player-expence");
+  const perPlayerExpence = getInputFieldValueById('per-player-expence');
 
-  const expencesAsPlayerNum = perPlayerExpence * numberOfPlayer;
+  const expencesAsPlayerNum = perPlayerExpence * (numberOfPlayer + 1);
 
-  const playerExpences = document.getElementById("player-expences");
+  const playerExpences = document.getElementById('player-expences');
   playerExpences.innerText = expencesAsPlayerNum;
 });
+
+document.getElementById('calculate-total-btn').addEventListener('click', function(){
+    const playerExpences = getTextValueById('player-expences');
+    const managerExpence = getInputFieldValueById('manager-expence');
+    const couchExpence = getInputFieldValueById('couch-expence');
+    console.log(couchExpence);
+
+    const totalExpences = playerExpences + managerExpence + couchExpence;
+
+    const total = document.getElementById('total-expences');
+    total.innerText = totalExpences;
+
+})
